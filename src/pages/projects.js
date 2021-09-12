6469106e529fcda2818cced87e9ce3d16fcf45cf
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-09-12 22:08:28 +0100

    removed unecessary import

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-// import * as React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
