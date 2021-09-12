8908918b61991b227cf10923187c27c0560ff528
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-09-12 22:01:35 +0100

    comment out import temporarily

--- src/components/NavBurger.js
+++ src/components/NavBurger.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 import { Fragment, useState } from "react"
-import NavMenu from "./NavMenu"
+// import NavMenu from "./NavMenu"
 
 import * as styles from "../styles/NavBurger.module.css"
 
