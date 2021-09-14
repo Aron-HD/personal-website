f58bdabbc38fd3d458e23857d75861c47b9870b7
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-09-14 19:34:16 +0100

    replaced navbar with closed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,14 @@
-import { Link } from "gatsby"
 import React from "react"
-// import Logo from "./Logo"
-// import styled from "styled-components"
+import NavBurger from "./NavBurger"
 
-import * as styles from "../styles/Navbar.module.css"
+import * as styles from "../styles/NavClosed.module.css"
 
-export default function Navbar() {
+const NavClosed = () => {
   return (
     <nav className={styles.navbar}>
-      {/* <Logo className={styles.logo} /> */}
-      <input className={styles.menuBtn} type="checkbox" id={styles.menuBtn} />
-      <label className={styles.menuIcon} for={styles.menuBtn}>
-        <span className={styles.navicon}></span>
-      </label>
-      <ul className={styles.menu}>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/projects">Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        {/* <li>
-          <Link to="/blogs">Blogs</Link>
-        </li>
-        <li>
-          <Link to="/create">New blog</Link>
-        </li> */}
-      </ul>
+      <NavBurger />
     </nav>
   )
 }
+
+export default NavClosed
