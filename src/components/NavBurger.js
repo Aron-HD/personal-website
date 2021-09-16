1fdd5c6e0e3ba481f156b9364bcb162dad16820f
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-09-17 00:31:03 +0100

    converted to styled component

--- src/components/NavBurger.js
+++ src/components/NavBurger.js
@@ -2,18 +2,47 @@
 import { jsx } from "theme-ui"
 import { Fragment, useState } from "react"
 import NavMenu from "./NavMenu"
+import styled from "styled-components"
 
-import * as styles from "../styles/NavBurger.module.css"
+const StyledBurger = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-flow: column nowrap;
+  width: 2rem;
+  height: 2rem;
+  /* position: fixed; */
+  top: 40px;
+  right: 40px;
+  z-index: 20;
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    border-radius: 10px;
+    transform-origin: 1px;
+    transition: all 0.2s linear;
+    &:nth-child(1) {
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+    }
+    &:nth-child(2) {
+      transform: ${({ open }) => (open ? "translateX(100%)" : "translateX(0)")};
+      opacity: ${({ open }) => (open ? 0 : 1)};
+    }
+    &:nth-child(3) {
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+    }
+  }
+`
 
 const NavBurger = () => {
   const [open, setOpen] = useState(false)
   return (
     <Fragment>
-      <button id={styles.hamburger} open={open} onClick={() => setOpen(!open)}>
-        <span sx={{ bg: "primary" }} />
-        <span sx={{ bg: "primary" }} />
-        <span sx={{ bg: "primary" }} />
-      </button>
+      <StyledBurger open={open} onClick={() => setOpen(!open)}>
+        <div sx={{ bg: "primary" }} />
+        <div sx={{ bg: "primary" }} />
+        <div sx={{ bg: "primary" }} />
+      </StyledBurger>
       <NavMenu open={open} />
     </Fragment>
   )
