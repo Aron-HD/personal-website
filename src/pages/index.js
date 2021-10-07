960b85f12d009be2c8cc11e4d4035ccc96c1a283
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-08 00:48:17 +0100

    added style module and wrapped in container
    - added css module to deal with layout of logo
    - wrapped in container for spacing
    - wrote media query for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,11 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 
-import "../styles/global.css"
+import * as styles from "../styles/index.module.css"
 
 const IndexPage = () => (
   <Layout>
+    <div className={styles.container}>
       <Seo title="Home" />
       <h2 sx={{ variant: "styles.logo" }}>Software Developer</h2>
       <h3 sx={{ variant: "styles.h3" }}>Portfolio & blog site</h3>
@@ -21,11 +22,12 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     /> */}
-    <p sx={{ variant: "styles.p" }}>
-      <button sx={{ variant: "styles.button" }}>
-        <Link to="/projects/">View projects</Link>
-      </button>
-    </p>
+      <p sx={{ variant: "styles.p" }}>
+        <button sx={{ variant: "styles.button" }}>
+          <Link to="/projects/">View projects</Link>
+        </button>
+      </p>
+    </div>
   </Layout>
 )
 
