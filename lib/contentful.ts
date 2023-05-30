ef17b0eb8e272a4259c887493cf827c5255959be
Aron Davidson <Davidson.Aron@adlittle.com>
2023-05-30 13:27:34 +0100

    add contentful

---
+++ lib/contentful.ts
@@ -0,0 +1,33 @@
+import { createClient, EntrySkeletonType, ContentfulClientApi, Entry } from 'contentful';
+
+export default class ContentService {
+
+  _client: ContentfulClientApi<undefined>;
+
+  /**
+   *
+   */
+  constructor() {
+    this._client = this.getClient();
+  }
+
+  getClient() {
+    return createClient({
+      space: process.env.CONTENTFUL_SPACE_ID || '',
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+    })
+  }
+
+  async getMe() {
+    return await this._client.getEntry<PersonEntrySkeleton>("15jwOBqpxqSAOy2eOO4S0m");
+  };
+
+  async getEntriesByType<T extends EntrySkeletonType>(type: string) {
+    return (
+      await this._client.getEntries<T>({
+        content_type: type,
+      })
+    ).items;
+  };
+}
+
